fcaf9f732936536afd6d10493c2842770e618b1e
gustavomeloo <gugamelo2@gmail.com>
2022-07-11 22:25:35 -0300

    start

---
+++ src/main/java/br/com/sdconecta/SdconectaApplication.java
@@ -0,0 +1,13 @@
+package br.com.sdconecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdconectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdconectaApplication.class, args);
+	}
+
+}
